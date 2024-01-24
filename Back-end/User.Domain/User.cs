8d55d14587282af7c34240fbc261e8b0f525c2b0
Nguyễn Đình Sơn <120614021+dihson103@users.noreply.github.com>
2024-01-24 17:43:49 +0700

    create project

---
+++ Back-end/User.Domain/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User.Domain
+{
+    public class User
+    {
+        [Key]
+        public string Id { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public Role Role { get; set; } = Role.USER;
+
+        [Required]
+        public bool Status { get; set; } = true;
+    }
+}
